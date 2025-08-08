36ca8e3c85754033cff5697ac2d679a71d689f97
nithi1233 <nithimaran1233@gmail.com>
2025-08-08 23:47:29 +0530

    bug fixed and change new style for skill page

--- js/script.js
+++ js/script.js
@@ -49,27 +49,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Form submission
-const contactForm = document.getElementById('contactForm');
-if (contactForm) {
-    contactForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-        
-        // Get form values
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const subject = document.getElementById('subject').value;
-        const message = document.getElementById('message').value;
-        
-        // Here you would typically send the form data to a server
-        // For this example, we'll just log it and show an alert
-        console.log({ name, email, subject, message });
-        
-        alert('Thank you for your message! I will get back to you soon.');
-        contactForm.reset();
-    });
-}
-
 // Animation on scroll
 window.addEventListener('scroll', revealOnScroll);
 
